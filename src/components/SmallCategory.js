1b250bd361bddcec01ae742d4bd3d8f058f04b75
comolove <jungjinwoo41@gmail.com>
2023-04-17 13:01:24 +0900

    feat: add Header and install react bootstrap

---
+++ src/components/SmallCategory.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './css/SmallCategory.module.css';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
+const SmallCategory = ({ prop }) => {
+    if (prop == 1) return (
+      <ButtonGroup className={styles.btn_group}>
+        <Button className={styles.btn_small}>한식</Button>
+        <Button className={styles.btn_small}>중식</Button>
+        <Button className={styles.btn_small}>일식</Button>
+        <Button className={styles.btn_small}>양식</Button>
+        <Button className={styles.btn_small}>분식</Button>
+      </ButtonGroup>)
+
+    else return (
+      <ButtonGroup className={styles.btn_group}>
+        <Button className={styles.btn_small}>pc방</Button>
+        <Button className={styles.btn_small}>노래방</Button>
+        <Button className={styles.btn_small}>당구장</Button>
+        <Button className={styles.btn_small}>영화</Button>
+      </ButtonGroup>);
+};
+  
+export default SmallCategory;

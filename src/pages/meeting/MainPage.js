aa971cf17bb01b46984b545b8ba60ddc388bb32e
comolove <jungjinwoo41@gmail.com>
2023-04-18 11:13:20 +0900

    Apply .gitignore

--- src/pages/meeting/MainPage.js
+++ src/pages/meeting/MainPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Container from 'react-bootstrap/Container';
 import CardGroup from 'react-bootstrap/CardGroup';
 import CardInfo from '../../components/CardInfo';
@@ -8,11 +8,11 @@ import FirebaseService from '../../services/firebase_service';
 const MainPage = () => {
   const firebase = new FirebaseService();
 
-  // const [cards, setCards] = useState();
-
-  firebase.getMeetings().then((values) => {
-    console.log(values);
-  });
+  useEffect(() => {
+    firebase.getMeetings().then((values) => {
+      console.log(values);
+    });
+  },[]);
 
   return (
     <>

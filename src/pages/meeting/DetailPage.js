c0166d0c966cac0de7fadeba0967108a6151bf4e
jiyoung choi <qq801@naver.com>
2023-04-18 22:57:11 +0900

    feat: tag cascader

--- src/pages/meeting/DetailPage.js
+++ src/pages/meeting/DetailPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Participant from '../../components/Participant';
+import  '../../css/pages/DetailPage.css';
 const DetailPage = (props) => {
     return (
-        <div>
+           <div>
             <h3>미팅 상세보기 </h3>
             <Participant/>
         </div>
-    );
+    )
 }
 export default DetailPage;

2947dd9e89c09e2e385df58dafa2ef5950ab2df1
jiyoung choi <qq801@naver.com>
2023-04-19 12:05:23 +0900

    style: DetailPage components location,
    위치 조정
    목록과 참여 버튼 추가

--- src/pages/meeting/DetailPage.js
+++ src/pages/meeting/DetailPage.js
@@ -1,8 +1,10 @@
+
 import React from 'react';
 import styles from '../../css/pages/DetailPage.module.css';
 import Card from 'react-bootstrap/Card';
 import Participant from '../../components/Participant';
 import Badge from 'react-bootstrap/Badge';
+import { Link } from 'react-router-dom';
 
 const info = {//테스트용 객체
     content:"오늘 밤 치킨 먹어요! 양념 후라이드 다 쌉가능",
@@ -14,28 +16,36 @@ const info = {//테스트용 객체
     type:"식사"
 };
 
-
 const DetailPage = (props) => {
-    return (
-    <Card>
-        <Card.Body>
-          <Card.Title>{info.title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">게시자명</Card.Subtitle>
-          <Card.Text>{info.content}</Card.Text>
-          <Badge bg="primary">{info.type}</Badge>{' '}
-          <Badge bg="info">{info.tag}</Badge>{' '}
-          <Card.Link href={info.link}>오픈채팅 링크</Card.Link>
-        </Card.Body>
+  return (
+    <div class="board-container">
+
+        <Card className={styles.title2}>{info.title}</Card>
+        <Card.Subtitle className="mb-2 text-muted">게시자명</Card.Subtitle>
+        <Card className={styles.content2}>{info.content}</Card>
+
+      <div style={{display: 'flex', flexDirection: 'column',  justifyContent: 'center'}}>
+        <div class ="info-box">    
+          <Badge bg="primary"className={styles.badgetag}>{info.type}</Badge>{' '}
+          <Badge bg="info"className={styles.badgetag}>{info.tag}</Badge>{' '}
+          
+          <Badge bg="info" className={styles.badge2}>장소</Badge>
+          <Card className={styles.text2}>{info.place}</Card>
+          <Badge bg="info" className={styles.badge3}>시간</Badge>
+          <Card className={styles.text2}>{info.time}</Card>
+        </div>
         
-        <Badge bg="info" className={styles.badge2}>장소</Badge>
-        <Card className={styles.text2}>{info.place}</Card>
-        <Badge bg="info" className={styles.badge2}>시간</Badge>
-        <Card className={styles.text2}>{info.time}</Card>
-        <Participant />
+        <Card className={styles.linkbox2}>
+          <Card.Link className={styles.link2} href={info.link}>오픈채팅 링크</Card.Link>    
+        </Card>
+      </div>
+      <Participant />
+      <div class="form-buttons">
+          <button class = "main-button"><Link className = "link-main" to ={'/main'}>목록</Link></button>
+          <button type="submit" class="submit-button">참여</button>
+      </div>
 
-      </Card>
-    
-    );
-
+  </div>
+  )
 }
-export default DetailPage;
+export default DetailPage;  
